1f5f9674483168599c4ee8c01da7886118d7bae4
apoorvsadana <95699312+apoorvsadana@users.noreply.github.com>
2024-04-11 19:00:00 +0530

    delete all

--- eth_event.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while true;
- do curl http://localhost:8545 -X POST -H "Content-Type: application/json" --data '{"method":"eth_getLogs","params":[{"address": "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"}],"id":1,"jsonrpc":"2.0"}';
- sleep 1;
-done
