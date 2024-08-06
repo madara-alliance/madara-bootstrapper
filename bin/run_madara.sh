959ea0c5d58f2f388133ad91cf3964c4981af546
Arun Jangra <arunjangra1001@gmail.com>
2024-08-06 19:46:39 +0530

    feat : updated commands for background run

--- bin/run_madara.sh
+++ bin/run_madara.sh
@@ -3,4 +3,4 @@
 # This binary is build using the disabled fee flag on madara.
 
 ./madara setup --from-local ./configs --base-path=./setup_dir --chain=dev
-RUST_LOG=runtime::starknet ./madara --dev --tx-ban-seconds=0 --pruning=archive --base-path=./setup_dir --settlement=Ethereum --settlement-conf eth-config.json --rpc-cors all
+RUST_LOG=runtime::starknet ./madara --dev --tx-ban-seconds=0 --pruning=archive --base-path=./setup_dir --settlement=Ethereum --settlement-conf eth-config.json --rpc-cors all </dev/null &>/dev/null &
