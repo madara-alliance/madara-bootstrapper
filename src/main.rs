bdc8b80d700715b8b6e692efb48103ea196a492a
mohiiit <mohit18343@iiitd.ac.in>
2024-11-04 18:22:39 +0530

    fix: port of madara fixed in default

--- src/main.rs
+++ src/main.rs
@@ -88,7 +88,7 @@ impl Default for ConfigFile {
         Self {
             eth_rpc: "http://127.0.0.1:8545".to_string(),
             eth_priv_key: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80".to_string(),
-            rollup_seq_url: "http://127.0.0.1:9944".to_string(),
+            rollup_seq_url: "http://127.0.0.1:19944".to_string(),
             rollup_priv_key: "0xabcd".to_string(),
             eth_chain_id: 31337,
             l1_deployer_address: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266".to_string(),
